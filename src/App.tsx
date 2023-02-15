06a02c1648c28dafdd1f650e92417d52a6369de2
Roman Moisyeyev <roman.moisyeyev@gmail.com>
2023-02-15 09:05:16 +0200

    component-01

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,50 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    return (
+        <div className="App">
+            this is app component
+            <Rating/>
+            <Accordion/>
+
+        </div>
+    );
+}
+
+export const Rating = () => {
+
+    return (
+        <div>
+           <Star/>
+           <Star/>
+           <Star/>
+           <Star/>
+           <Star/>
+        </div>
+    )
+}
+
+export const Star =()=>{
+    return(
+        <div>star</div>
+    )
+}
+
+export const Accordion = () => {
+    return (
+        <div>
+            <h3>MENU</h3>
+            <ul>
+                <li>1</li>
+                <li>2</li>
+                <li>3</li>
+            </ul>
+        </div>
+    )
 }
 
 export default App;
